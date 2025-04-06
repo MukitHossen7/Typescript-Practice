2a0fd56f62a421eb04793f79a4683c76b399d284
MukitHossen7 <hossenmukit7@gmail.com>
2025-04-06 15:12:08 +0600

    Refactor variable and object declarations, update logging for names and arrays

--- basic-typescript/output/script.js
+++ basic-typescript/output/script.js
@@ -1,44 +1,18 @@
 "use strict";
-//variable declaration
-let name1 = "John Doe";
-console.log(name1);
-let age = 30;
-console.log(age);
-age = "20";
-console.log(age);
-age = true;
-console.log(age);
-//function declaration
-function add(a, b) {
-    return a + b;
-}
-console.log(add(2, 3));
-//object declaration
-const person = {
+let names = "John Doe";
+console.log(names);
+names = 12345;
+console.log(names);
+names = true;
+console.log(names);
+const arr = [1, 2, 3, 4, 5];
+arr.push("Hello");
+arr.push(true);
+console.log(arr);
+const obj = {
     name: "John Doe",
     age: 30,
-    isStudent: false,
-    greet: function () {
-        console.log(`Hello, my name is ${this.name}`);
-    },
+    isActive: true,
 };
-person.greet();
-person.name = "Mustafa";
-console.log(person);
-//array declaration`
-const numbers = [1, 2, 3, 4, 5];
-numbers.push(6);
-console.log(numbers);
-const names = ["John", "Jane", "Doe"];
-names.push("Mustafa");
-console.log(names);
-const mixedArray = [
-    1,
-    "John",
-    true,
-    { name: "Doe" },
-    function () {
-        console.log(`Hello, my name is`);
-    },
-];
-console.log(mixedArray);
+obj.name = 25;
+console.log(obj);
--- basic-typescript/src/script.ts
+++ basic-typescript/src/script.ts
@@ -1,55 +1,19 @@
-//variable declaration
-let name1: string = "John Doe";
-console.log(name1);
-
-let age: number | string | boolean = 30;
-console.log(age);
-age = "20";
-console.log(age);
-age = true;
-console.log(age);
-
-//function declaration
-function add(a: number, b: number): number {
-  return a + b;
-}
-console.log(add(2, 3));
-
-//object declaration
-
-const person: {
-  name: string;
-  age: number;
-  isStudent: boolean;
-  greet: Function;
-} = {
-  name: "John Doe",
-  age: 30,
-  isStudent: false,
-  greet: function () {
-    console.log(`Hello, my name is ${this.name}`);
-  },
-};
-person.greet();
-person.name = "Mustafa";
-console.log(person);
-
-//array declaration`
-const numbers: number[] = [1, 2, 3, 4, 5];
-numbers.push(6);
-console.log(numbers);
-
-const names: string[] = ["John", "Jane", "Doe"];
-names.push("Mustafa");
+let names: any = "John Doe";
+console.log(names);
+names = 12345;
+console.log(names);
+names = true;
 console.log(names);
 
-const mixedArray: (number | string | boolean | object | Function)[] = [
-  1,
-  "John",
-  true,
-  { name: "Doe" },
-  function () {
-    console.log(`Hello, my name is`);
-  },
-];
-console.log(mixedArray);
+const arr: any[] = [1, 2, 3, 4, 5];
+arr.push("Hello");
+arr.push(true);
+console.log(arr);
+
+const obj: { name: any; age: any; isActive: any } = {
+  name: "John Doe",
+  age: 30,
+  isActive: true,
+};
+obj.name = 25;
+console.log(obj);
