bf4c4cd74b7719364c87c77ca7a5f595b9e12014
MukitHossen7 <hossenmukit7@gmail.com>
2025-04-06 09:33:27 +0600

    Refactor: Comment out unused code and maintain variable logging

--- basic-typescript/output/script.js
+++ basic-typescript/output/script.js
@@ -1,19 +1,26 @@
 "use strict";
-console.log("Hello, world!");
-const greeting = "Hello, TypeScript!";
-console.log(greeting);
-const country = () => {
-    console.log("Bangladesh");
-};
-country();
-const person = {
-    name: "John Doe",
-    age: 30,
-    greet: function () {
-        console.log("Hello, " + this.name);
-    },
-};
-person.greet();
-console.log(person);
-const numbers = [1, 2, 3, 4, 5];
-console.log(numbers);
+// console.log("Hello, world!");
+// const greeting = "Hello, TypeScript!";
+// console.log(greeting);
+// const country = () => {
+//   console.log("Bangladesh");
+// };
+// country();
+// const person = {
+//   name: "John Doe",
+//   age: 30,
+//   greet: function () {
+//     console.log("Hello, " + this.name);
+//   },
+// };
+// person.greet();
+// console.log(person);
+// const numbers = [1, 2, 3, 4, 5];
+// console.log(numbers);
+let name1 = "John Doe";
+console.log(name1);
+name1 = "Jane Smith";
+console.log(name1);
+let age = 30;
+console.log(age);
+// This will cause a TypeScript error
