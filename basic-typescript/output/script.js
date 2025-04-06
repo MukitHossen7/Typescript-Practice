8cfbb56f13fb59f811b3ffc36d609e3fa55f3332
MukitHossen7 <hossenmukit7@gmail.com>
2025-04-06 10:56:08 +0600

    use explicit in variable

--- basic-typescript/output/script.js
+++ basic-typescript/output/script.js
@@ -1,47 +1,10 @@
 "use strict";
-// console.log("Hello, world!");
-// const greeting = "Hello, TypeScript!";
-// console.log(greeting);
-// const country = () => {
-//   console.log("Bangladesh");
-// };
-// country();
-// const person = {
-//   name: "John Doe",
-//   age: 30,
-//   greet: function () {
-//     console.log("Hello, " + this.name);
-//   },
-// };
-// person.greet();
-// console.log(person);
-// const numbers = [1, 2, 3, 4, 5];
-// console.log(numbers);
+//variable declaration
 let name1 = "John Doe";
-console.log(name1);
-name1 = "Jane Smith";
 console.log(name1);
 let age = 30;
 console.log(age);
-// This will cause a TypeScript error
-function multiply(x, y) {
-    return x * y;
-}
-console.log(multiply(5, 10)); // Output: 50
-const fruits = ["apple", "banana", "orange"];
-console.log(fruits);
-fruits.push("grape");
-console.log(fruits);
-const number = [1, 2, 3, 4, 5];
-console.log(number);
-number.push(6);
-console.log(number);
-// This will cause a TypeScript error
-const person = {
-    name: "John Doe",
-    age: 30,
-    status: "active",
-};
-console.log(person);
-person.name = "Jane Smith";
-console.log(person);
+age = "20";
+console.log(age);
+age = true;
+console.log(age);
--- basic-typescript/src/script.ts
+++ basic-typescript/src/script.ts
@@ -1,54 +1,10 @@
-// console.log("Hello, world!");
-// const greeting = "Hello, TypeScript!";
-// console.log(greeting);
-
-// const country = () => {
-//   console.log("Bangladesh");
-// };
-// country();
-
-// const person = {
-//   name: "John Doe",
-//   age: 30,
-//   greet: function () {
-//     console.log("Hello, " + this.name);
-//   },
-// };
-// person.greet();
-// console.log(person);
-
-// const numbers = [1, 2, 3, 4, 5];
-// console.log(numbers);
-
-let name1 = "John Doe";
-console.log(name1);
-name1 = "Jane Smith";
+//variable declaration
+let name1: string = "John Doe";
 console.log(name1);
 
-let age = 30;
+let age: number | string | boolean = 30;
 console.log(age);
-// This will cause a TypeScript error
-
-function multiply(x: number, y: number): number {
-  return x * y;
-}
-console.log(multiply(5, 10)); // Output: 50
-
-const fruits = ["apple", "banana", "orange"];
-console.log(fruits);
-fruits.push("grape");
-console.log(fruits);
-
-const number = [1, 2, 3, 4, 5];
-console.log(number);
-number.push(6);
-console.log(number);
-// This will cause a TypeScript error
-const person = {
-  name: "John Doe",
-  age: 30,
-  status: "active",
-};
-console.log(person);
-person.name = "Jane Smith";
-console.log(person);
+age = "20";
+console.log(age);
+age = true;
+console.log(age);
