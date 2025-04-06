21b42cab8fd35de6d55bcad434a3e73c25613444
MukitHossen7 <hossenmukit7@gmail.com>
2025-04-06 14:43:32 +0600

    Add person object and array declarations with logging in script

--- basic-typescript/output/script.js
+++ basic-typescript/output/script.js
@@ -14,3 +14,31 @@ function add(a, b) {
 }
 console.log(add(2, 3));
 //object declaration
+const person = {
+    name: "John Doe",
+    age: 30,
+    isStudent: false,
+    greet: function () {
+        console.log(`Hello, my name is ${this.name}`);
+    },
+};
+person.greet();
+person.name = "Mustafa";
+console.log(person);
+//array declaration`
+const numbers = [1, 2, 3, 4, 5];
+numbers.push(6);
+console.log(numbers);
+const names = ["John", "Jane", "Doe"];
+names.push("Mustafa");
+console.log(names);
+const mixedArray = [
+    1,
+    "John",
+    true,
+    { name: "Doe" },
+    function () {
+        console.log(`Hello, my name is`);
+    },
+];
+console.log(mixedArray);
--- basic-typescript/src/script.ts
+++ basic-typescript/src/script.ts
@@ -16,3 +16,40 @@ function add(a: number, b: number): number {
 console.log(add(2, 3));
 
 //object declaration
+
+const person: {
+  name: string;
+  age: number;
+  isStudent: boolean;
+  greet: Function;
+} = {
+  name: "John Doe",
+  age: 30,
+  isStudent: false,
+  greet: function () {
+    console.log(`Hello, my name is ${this.name}`);
+  },
+};
+person.greet();
+person.name = "Mustafa";
+console.log(person);
+
+//array declaration`
+const numbers: number[] = [1, 2, 3, 4, 5];
+numbers.push(6);
+console.log(numbers);
+
+const names: string[] = ["John", "Jane", "Doe"];
+names.push("Mustafa");
+console.log(names);
+
+const mixedArray: (number | string | boolean | object | Function)[] = [
+  1,
+  "John",
+  true,
+  { name: "Doe" },
+  function () {
+    console.log(`Hello, my name is`);
+  },
+];
+console.log(mixedArray);
