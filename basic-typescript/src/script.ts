a533674ba9d11265ee28c941a497bd20a7c51678
MukitHossen7 <hossenmukit7@gmail.com>
2025-04-06 09:09:28 +0600

    first commit

---
+++ basic-typescript/src/script.ts
@@ -0,0 +1,18 @@
+console.log("Hello, world!");
+const greeting = "Hello, TypeScript!";
+console.log(greeting);
+
+const country = () => {
+  console.log("Bangladesh");
+};
+country();
+
+const person = {
+  name: "John Doe",
+  age: 30,
+  greet: function () {
+    console.log("Hello, " + this.name);
+  },
+};
+person.greet();
+console.log(person);
