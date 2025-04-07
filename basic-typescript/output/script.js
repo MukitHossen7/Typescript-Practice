78274d3fb20439ba97c9ba914c1c30105877ffd4
MukitHossen7 <hossenmukit7@gmail.com>
2025-04-07 22:21:46 +0600

    Add myFunction and myNames functions with console logging

--- basic-typescript/output/script.js
+++ basic-typescript/output/script.js
@@ -7,3 +7,11 @@ const add2 = (a, b, c = 0) => {
     return a + b + c;
 };
 console.log(add2(10, 15));
+const myFunction = () => {
+    console.log("Hello World");
+};
+myFunction();
+const myNames = (a, b) => {
+    console.log(`${a} ${b}`);
+};
+myNames("Hello", "World");
--- basic-typescript/src/script.ts
+++ basic-typescript/src/script.ts
@@ -7,3 +7,14 @@ const add2 = (a: number, b: number, c: number = 0): number => {
   return a + b + c;
 };
 console.log(add2(10, 15));
+
+const myFunction: Function = () => {
+  console.log("Hello World");
+};
+myFunction();
+
+const myNames = (a: string, b: string) => {
+  console.log(`${a} ${b}`);
+};
+
+myNames("Hello", "World");
