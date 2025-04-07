8b7edbc9c3f5fd205ad4ddacde469b1d9366fbb4
MukitHossen7 <hossenmukit7@gmail.com>
2025-04-07 21:12:43 +0600

    Remove variable and object declarations, along with associated logging

--- basic-typescript/src/script.ts
+++ basic-typescript/src/script.ts
@@ -1,19 +1,1 @@
-let names: any = "John Doe";
-console.log(names);
-names = 12345;
-console.log(names);
-names = true;
-console.log(names);
 
-const arr: any[] = [1, 2, 3, 4, 5];
-arr.push("Hello");
-arr.push(true);
-console.log(arr);
-
-const obj: { name: any; age: any; isActive: any } = {
-  name: "John Doe",
-  age: 30,
-  isActive: true,
-};
-obj.name = 25;
-console.log(obj);
