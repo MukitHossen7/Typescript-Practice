83e1da11ca753b105f93bedac570c8bc3ca8ee36
MukitHossen7 <hossenmukit7@gmail.com>
2025-04-07 21:57:33 +0600

    Update add function to include optional third parameter and add logging for arrow function

--- basic-typescript/output/script.js
+++ basic-typescript/output/script.js
@@ -1,5 +1,9 @@
 "use strict";
-function add(a, b) {
-    return a + b;
+function add(a, b, c = 0) {
+    return a + b + c;
 }
 console.log(add(10, 15));
+const add2 = (a, b, c = 0) => {
+    return a + b + c;
+};
+console.log(add2(10, 15));
--- basic-typescript/src/script.ts
+++ basic-typescript/src/script.ts
@@ -1,4 +1,9 @@
-function add(a: number, b: number) {
-  return a + b;
+function add(a: number, b: number, c: number = 0): number {
+  return a + b + c;
 }
 console.log(add(10, 15));
+
+const add2 = (a: number, b: number, c: number = 0): number => {
+  return a + b + c;
+};
+console.log(add2(10, 15));
