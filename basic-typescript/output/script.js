f6974da2ef54da8cdda96e1b64912ca4fd90cf30
MukitHossen7 <hossenmukit7@gmail.com>
2025-04-07 21:16:28 +0600

    Add function declaration for addition and log its output

--- basic-typescript/output/script.js
+++ basic-typescript/output/script.js
@@ -1,18 +1,5 @@
 "use strict";
-let names = "John Doe";
-console.log(names);
-names = 12345;
-console.log(names);
-names = true;
-console.log(names);
-const arr = [1, 2, 3, 4, 5];
-arr.push("Hello");
-arr.push(true);
-console.log(arr);
-const obj = {
-    name: "John Doe",
-    age: 30,
-    isActive: true,
-};
-obj.name = 25;
-console.log(obj);
+function add(a, b) {
+    return a + b;
+}
+console.log(add(10, 15));
