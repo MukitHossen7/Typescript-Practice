2cb6c3cf95894a416f840607619a77a6685926e3
MukitHossen7 <hossenmukit7@gmail.com>
2025-04-08 10:55:08 +0600

    Refactor script.ts to comment out unused code and enhance the Person class with proper type definitions

--- basic-typescript/output/script.js
+++ basic-typescript/output/script.js
@@ -1,12 +1,38 @@
 "use strict";
-let add;
-add = (a, b) => {
-    return a + b;
-};
-console.log(add(2, 3)); // Output: 5"));
-const person = {
-    name: "John",
-    age: 30,
-    email: "mim@gmail.com",
-};
-console.log(person);
+// type userInfo = {
+//   name: string;
+//   age: number | string;
+//   email: string;
+// };
+// let add: (a: number, b: number) => number;
+// add = (a, b) => {
+//   return a + b;
+// };
+// console.log(add(2, 3)); // Output: 5"));
+// const person: userInfo = {
+//   name: "John",
+//   age: 30,
+//   email: "mim@gmail.com",
+// };
+// console.log(person);
+class Person {
+    constructor(name, age, country) {
+        this.name = name;
+        this.age = age;
+        this.country = country;
+    }
+    user() {
+        return `Name: ${this.name}, Age: ${this.age}, Country: ${this.country}`;
+    }
+}
+const person1 = new Person("John", "30", "USA");
+console.log(person1);
+console.log(person1.user());
+const person2 = new Person("Jane", 25, "Canada");
+console.log(person2);
+console.log(person2.user());
+const userArray = [];
+userArray.push("John");
+userArray.push("Jane");
+userArray.push(30);
+console.log(userArray);
--- basic-typescript/src/script.ts
+++ basic-typescript/src/script.ts
@@ -1,16 +1,42 @@
-type userInfo = {
+// type userInfo = {
+//   name: string;
+//   age: number | string;
+//   email: string;
+// };
+// let add: (a: number, b: number) => number;
+// add = (a, b) => {
+//   return a + b;
+// };
+// console.log(add(2, 3)); // Output: 5"));
+// const person: userInfo = {
+//   name: "John",
+//   age: 30,
+//   email: "mim@gmail.com",
+// };
+// console.log(person);
+
+class Person {
   name: string;
   age: number | string;
-  email: string;
-};
-let add: (a: number, b: number) => number;
-add = (a, b) => {
-  return a + b;
-};
-console.log(add(2, 3)); // Output: 5"));
-const person: userInfo = {
-  name: "John",
-  age: 30,
-  email: "mim@gmail.com",
-};
-console.log(person);
+  country: string;
+  constructor(name: string, age: number | string, country: string) {
+    this.name = name;
+    this.age = age;
+    this.country = country;
+  }
+  user(): string {
+    return `Name: ${this.name}, Age: ${this.age}, Country: ${this.country}`;
+  }
+}
+const person1 = new Person("John", "30", "USA");
+console.log(person1);
+console.log(person1.user());
+const person2 = new Person("Jane", 25, "Canada");
+console.log(person2);
+console.log(person2.user());
+
+const userArray: (string | number)[] = [];
+userArray.push("John");
+userArray.push("Jane");
+userArray.push(30);
+console.log(userArray);
