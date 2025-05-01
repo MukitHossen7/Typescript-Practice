5f41572be38dec5aef08255295c0b06dece9393a
MukitHossen7 <hossenmukit7@gmail.com>
2025-05-01 23:07:26 +0600

    Refactor tocn.ts by adding age checks, user validation, and person type definition with console logs

--- typescript-module-1/module/src/tocn.ts
+++ typescript-module-1/module/src/tocn.ts
@@ -1,2 +1,23 @@
-console.log("Employee");
-console.log("Employee");
+{
+  //Ternary Operator
+  let age: number = 17;
+  const checkAge = age >= 18 ? "Adult" : "Minor";
+  console.log(checkAge);
+
+  // Nullish Coalescing Operator
+  const isUser: null | undefined | string = "Leonardo";
+  const checkValid = isUser ?? "He is Hacker";
+  console.log(checkValid);
+  type Person = {
+    name: string;
+    age: number | string;
+    email?: string;
+    phoneNumber?: string;
+  };
+  const person: Person = {
+    name: "John Doe",
+    age: 25,
+    email: "john@gmail.com",
+  };
+  console.log(person);
+}
