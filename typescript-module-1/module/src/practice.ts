3a05489b679729bc7ed27a0129ba0128ba2dfd08
MukitHossen7 <hossenmukit7@gmail.com>
2025-05-02 01:51:46 +0600

    Refactor practice.ts by uncommenting type definitions and example functions for clarity

--- typescript-module-1/module/src/practice.ts
+++ typescript-module-1/module/src/practice.ts
@@ -1,6 +1,6 @@
 {
-  //   type MixArray = (string | number | boolean)[];
-  //   const mixArray: MixArray = ["Mukit", 15, true, "Bangladesh", 25, false];
+  // type MixArray = (string | number | boolean)[];
+  // const mixArray: MixArray = ["Mukit", 15, true, "Bangladesh", 25, false];
   //   console.log(mixArray);
   //   const tupleArr: [number, string, boolean, string] = [
   //     15,
@@ -22,15 +22,34 @@
   //   };
   //   console.log(person);
 
-  type Add = (a: number, b: number) => number;
+  //   type Add = (a: number, b: number) => number;
 
-  const add: Add = (a, b) => {
-    return a + b;
+  //   const add: Add = (a, b) => {
+  //     return a + b;
+  //   };
+  //   add(5, 10); // 15
+
+  //   function add1(a: number, b: number): number {
+  //     return a + b;
+  //   }
+  //     console.log(add(5, 10)); // 15
+  // const age: number | string = 25;
+
+  type A = {
+    name: string;
+    age: number;
   };
-  add(5, 10); // 15
-
-  function add1(a: number, b: number): number {
-    return a + b;
-  }
-  console.log(add(5, 10)); // 15
+  type B = {
+    name: string;
+    age: number;
+  };
+  const one: A = {
+    name: "Mukit",
+    age: 25,
+  };
+  const two: B = {
+    name: "Raju",
+    age: 20,
+  };
+  type oneTow = A & B;
 }
