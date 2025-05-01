10d348a075b265a5254a3d938d1691f72ae5e385
MukitHossen7 <hossenmukit7@gmail.com>
2025-05-01 11:38:40 +0600

    Add basic type declarations for string, number, boolean, null, undefined, array, and tuple

---
+++ typescript-module-1/module/dist/basictype.js
@@ -0,0 +1,17 @@
+"use strict";
+//string
+let personName = "John Doe";
+personName = "Jane Doe";
+// number
+let age = 30;
+// boolean
+let isEmployed = true;
+// null
+let emptyValue = null;
+// undefined
+let notAssigned = undefined;
+//Array
+let numbers = [1, 2, 3, 4, 5];
+let friends = ["Alice", "Bob", "Charlie"];
+// Tuple
+let person = ["John Doe", 30, true];
