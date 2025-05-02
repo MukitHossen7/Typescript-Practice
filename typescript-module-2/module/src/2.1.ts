c862e15bc0159d06828341e85a03555082daa962
MukitHossen7 <hossenmukit7@gmail.com>
2025-05-02 22:20:25 +0600

    type assertion is practice

--- typescript-module-2/module/src/2.1.ts
+++ typescript-module-2/module/src/2.1.ts
@@ -1,1 +1,15 @@
-console.log("2.1.ts");
+{
+  let massage: any = "Hello World! This is a test message.";
+  const length = (massage as string).length;
+  console.log(length); // Output: 38
+
+  let someData: unknown = 12345;
+  let num = someData as number;
+  console.log(num * 3);
+
+  const getValue = (): any => {
+    return "Hello, TypeScript!";
+  };
+  let strLength = (getValue() as string).length;
+  console.log(strLength);
+}
