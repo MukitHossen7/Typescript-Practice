d36efb2d0fbe0436498472247d20feeeb4671350
MukitHossen7 <hossenmukit7@gmail.com>
2025-05-03 16:49:31 +0600

    Add getPropertyValue function to demonstrate generic constraints with keyof operator

--- typescript-module-2/module/src/2.7.ts
+++ typescript-module-2/module/src/2.7.ts
@@ -11,5 +11,19 @@
   const owner: Vehicle3 = "car";
   console.log(owner);
 
+  const getPropertyValue = <X, Y extends keyof X>(obj: X, key: Y) => {
+    return obj[key];
+  };
+  const user = {
+    name: "Raju Islam",
+    age: 26,
+    email: "raju@gmail.com",
+  };
+  const car = {
+    model: "TASLA",
+    year: 2024,
+  };
+  const result = getPropertyValue(car, "year");
+  console.log(result);
   //
 }
