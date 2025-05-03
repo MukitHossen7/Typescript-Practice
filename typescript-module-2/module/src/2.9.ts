963f7ead2bbdf4ff05a914704abaeb023a56447f
MukitHossen7 <hossenmukit7@gmail.com>
2025-05-03 20:06:35 +0600

    Add type definitions for vehicle properties and conditional checks in 2.9.ts

--- typescript-module-2/module/src/2.9.ts
+++ typescript-module-2/module/src/2.9.ts
@@ -4,5 +4,16 @@
   type A = checkString<string>;
   type B = checkString<boolean>;
 
+  type a1 = number;
+  type b1 = undefined;
+  type x = a1 extends null ? "true" : "false";
+
+  type Sheikh = {
+    bike: string;
+    car: string;
+    ship: string;
+  };
+  type CheckVehicle<T> = T extends keyof Sheikh ? "Yes" : "No";
+  type Has = CheckVehicle<"ship">;
   //
 }
