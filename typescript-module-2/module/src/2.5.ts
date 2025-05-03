97663bae34d0906af61fb16ebbf741a52ac49760
MukitHossen7 <hossenmukit7@gmail.com>
2025-05-03 12:52:43 +0600

    Add 2.5.ts to implement generic functions for array creation and value addition

---
+++ typescript-module-2/module/src/2.5.ts
@@ -0,0 +1,31 @@
+{
+  //function with generic
+
+  const createArray = <T>(para: T): T[] => {
+    return [para];
+  };
+  const value = createArray<string>("John Doe");
+  console.log(value);
+  const value1 = createArray<number>(12345);
+  console.log(value1);
+  const value2 = createArray<boolean>(true);
+  console.log(value2);
+
+  //   const add = <T extends number, Y extends number>(
+  //     num1: T,
+  //     num2: Y
+  //   ): number => {
+  //     return num1 * num2;
+  //   };
+  //   console.log(add<number, number>(10, 5));
+
+  const add = <T>(num1: T) => {
+    return num1;
+  };
+  const add1 = add<number>(10);
+  console.log(add1);
+  const add2 = add<string>("John Doe");
+  console.log(add2);
+
+  //
+}
