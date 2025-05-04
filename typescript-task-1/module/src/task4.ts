e203c27c4832f45f241bd2abdef447fd5b5791c8
MukitHossen7 <hossenmukit7@gmail.com>
2025-05-04 09:31:27 +0600

    Add Book and Magazine interfaces with union and intersection types in task4 module

--- typescript-task-1/module/src/task4.ts
+++ typescript-task-1/module/src/task4.ts
@@ -1,2 +1,28 @@
 {
+  interface Book {
+    bookName: string;
+    author: string;
+  }
+
+  interface Magazine {
+    price: number;
+    publishedYear: number;
+  }
+
+  type TUnion = Book | Magazine;
+  type TIntersection = Book & Magazine;
+
+  const obj1: TUnion = {
+    bookName: "Action",
+    author: "Mukit",
+  };
+
+  const obj2: TIntersection = {
+    bookName: "Romantic",
+    author: "Raju",
+    price: 1200,
+    publishedYear: 2020,
+  };
+  console.log(obj1);
+  console.log(obj2);
 }
