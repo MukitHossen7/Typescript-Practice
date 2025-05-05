79b13a8789bc58c3a7ee923d82e6a2a3d8d831f0
MukitHossen7 <hossenmukit7@gmail.com>
2025-05-06 00:57:25 +0600

    Add test method to CreditCardPayment class and instantiate user2 for additional payment demonstration

--- typescript-module-3/module/src/InterfaceAbstraction.ts
+++ typescript-module-3/module/src/InterfaceAbstraction.ts
@@ -6,7 +6,12 @@
     pay(amount: number): void {
       console.log(`Paid ${amount} by Credit Card`);
     }
+    test() {
+      console.log("this is ");
+    }
   }
   const user1 = new CreditCardPayment();
   user1.pay(500);
+  const user2 = new CreditCardPayment();
+  user2.pay(1000);
 }
