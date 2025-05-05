45479d37178a8dd59b016db83ce407e8ed7e608a
MukitHossen7 <hossenmukit7@gmail.com>
2025-05-06 00:10:05 +0600

    Add abstract Car class and TotaCar implementation to demonstrate abstraction in TypeScript

--- typescript-module-3/module/src/3.9.ts
+++ typescript-module-3/module/src/3.9.ts
@@ -22,6 +22,29 @@
   }
 
   const carVehicle = new CarVehicle();
-  carVehicle.move();
+  //   carVehicle.move();
+
+  abstract class Car2 {
+    abstract start(): void;
+    abstract stop(): void;
+    abstract move(): void;
+    test() {
+      console.log("i am testing");
+    }
+  }
+  class TotaCar extends Car2 {
+    start(): void {
+      console.log("I am starting the car engine");
+    }
+    stop(): void {
+      console.log("I am stopping the car engine");
+    }
+    move(): void {
+      console.log("I am moving the car");
+    }
+  }
+  const totaCar = new TotaCar();
+  totaCar.test();
+  totaCar.move();
   //
 }
