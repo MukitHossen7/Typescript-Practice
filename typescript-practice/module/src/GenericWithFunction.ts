fdf43bdadd00c11e9b9e838e122d3ea3ab95a625
MukitHossen7 <hossenmukit7@gmail.com>
2025-05-06 22:35:27 +0600

    Refactor GenericWithFunction.ts to comment out unused arrayGeneric function and its calls

--- typescript-practice/module/src/GenericWithFunction.ts
+++ typescript-practice/module/src/GenericWithFunction.ts
@@ -16,10 +16,24 @@
   //   console.log(identity<string>("Hello Mukit"));
   //   console.log(identity<number>(50));
 
-  const arrayGeneric = <T>(arr: T[]) => {
-    console.log(arr);
-    arr.forEach((value) => console.log(value));
+  //   const arrayGeneric = <T>(arr: T[]) => {
+  //     console.log(arr);
+  //     arr.forEach((value) => console.log(value));
+  //   };
+  //   arrayGeneric<string>(["Mukit", "Araf", "Nayeem"]);
+  //   arrayGeneric<number>([1, 2, 3, 4, 5]);
+
+  const processMixArray = <T>(arr: T[]) => {
+    arr.forEach((value) => {
+      if (typeof value === "string") {
+        console.log(`String: ${value.toLocaleUpperCase()}`);
+      } else if (typeof value === "number") {
+        console.log(`Number: ${value * 2}`);
+      } else if (typeof value === "boolean") {
+        console.log(`${value ? "Yes" : "No"}`);
+      }
+    });
   };
-  arrayGeneric<string>(["Mukit", "Araf", "Nayeem"]);
-  arrayGeneric<number>([1, 2, 3, 4, 5]);
+  type MixArray = string | number | boolean;
+  processMixArray<MixArray>(["Mukit", 25, true, "TypeScript", false, 100]);
 }
