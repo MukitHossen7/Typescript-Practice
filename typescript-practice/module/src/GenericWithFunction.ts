15bf4b3c679c2e9815ffa3e2594087cad91ea1cf
MukitHossen7 <hossenmukit7@gmail.com>
2025-05-06 22:25:05 +0600

    Refactor GenericWithFunction.ts to comment out unused identity function and its calls

--- typescript-practice/module/src/GenericWithFunction.ts
+++ typescript-practice/module/src/GenericWithFunction.ts
@@ -4,15 +4,22 @@
   //   };
   //   console.log(identity<string>("Hello Mukit"));
   //   console.log(identity<number>(50));
-  const identity = <T>(value: T): T => {
-    if (typeof value === "string") {
-      return value.toLocaleUpperCase() as T;
-    } else if (typeof value === "number") {
-      return (value * 2) as T;
-    } else {
-      return value;
-    }
+  //   const identity = <T>(value: T): T => {
+  //     if (typeof value === "string") {
+  //       return value.toLocaleUpperCase() as T;
+  //     } else if (typeof value === "number") {
+  //       return (value * 2) as T;
+  //     } else {
+  //       return value;
+  //     }
+  //   };
+  //   console.log(identity<string>("Hello Mukit"));
+  //   console.log(identity<number>(50));
+
+  const arrayGeneric = <T>(arr: T[]) => {
+    console.log(arr);
+    arr.forEach((value) => console.log(value));
   };
-  console.log(identity<string>("Hello Mukit"));
-  console.log(identity<number>(50));
+  arrayGeneric<string>(["Mukit", "Araf", "Nayeem"]);
+  arrayGeneric<number>([1, 2, 3, 4, 5]);
 }
