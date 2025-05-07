c3cacee04ec67d6350edfcf58cbfb1e65e0d4295
MukitHossen7 <hossenmukit7@gmail.com>
2025-05-07 22:30:19 +0600

    Refactor Conditional.ts to comment out the filterByRating function and its example usage

--- typescript-practice/module/src/Conditional.ts
+++ typescript-practice/module/src/Conditional.ts
@@ -9,3 +9,23 @@
 // console.log(formatString("Hello"));
 // console.log(formatString("Hello", true));
 // console.log(formatString("Hello", false));
+
+// Problem 2:
+// function filterByRating(
+//   items: { title: string; rating: number }[]
+// ): { title: string; rating: number }[] {
+//   const filterRating = items.filter((item) => item.rating >= 4);
+//   return filterRating;
+// }
+// const books = [
+//   { title: "Book A", rating: 4.5 },
+//   { title: "Book B", rating: 3.2 },
+//   { title: "Book C", rating: 5.0 },
+//   { title: "Book D", rating: 5.5 },
+//   { title: "Book E", rating: 3.9 },
+//   { title: "Book F", rating: 4.0 },
+//   { title: "Book G", rating: 4.1 },
+// ];
+// console.log(filterByRating(books));
+
+// Problem 3:
