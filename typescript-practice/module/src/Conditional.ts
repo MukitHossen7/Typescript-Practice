de90c0da7b181a54040c3ff64a616a566767e393
MukitHossen7 <hossenmukit7@gmail.com>
2025-05-07 22:05:51 +0600

    Refactor Conditional.ts to comment out unused type definitions and restore formatString function example

--- typescript-practice/module/src/Conditional.ts
+++ typescript-practice/module/src/Conditional.ts
@@ -1,22 +1,11 @@
-{
-  // type isString<T> = T extends string ? "Yes" : "No";
-  // type A = isString<string>;
-  // type B = isString<number>;
-  // type C = isString<boolean>;
-  // type IsObject<T> = T extends object ? "Object" : "Not Object";
-  // type A = IsObject<{}>;
-  // type B = IsObject<number>;
-
-  //ইউনিয়ন টাইপের সাথে ব্যবহার
-  // type ToString<T> = T extends number | boolean ? string : T
-  // type A = ToString<string>
-  // type B = ToString<number>
-
-  //এরেওর বা never টাইপ শনাক্ত করা
-  // type IsNever<T> = [T] extends [never] ? true : false;
-  // type A = IsNever<never>
-
-  //অ্যারে কিনা চেক করা
-  type IsArray<T> = T extends any[] ? true : false;
-  type A = IsArray<string[]>;
-}
+// Problem 1
+// function formatString(input: string, toUpper?: boolean): string {
+//   if (input && toUpper === false) {
+//     return input.toLocaleLowerCase();
+//   } else {
+//     return input.toLocaleUpperCase();
+//   }
+// }
+// console.log(formatString("Hello"));
+// console.log(formatString("Hello", true));
+// console.log(formatString("Hello", false));
