1bb14dbf2a6e3f474bc74014e0a0e4899b3e7f8c
MukitHossen7 <hossenmukit7@gmail.com>
2025-05-07 11:44:59 +0600

    Refactor Constraints.ts to update interface and function to use 'role' instead of 'name'

--- typescript-practice/module/src/Constraints.ts
+++ typescript-practice/module/src/Constraints.ts
@@ -13,14 +13,14 @@
 
   //Example : 2
   interface HasName {
-    name: string;
+    role: string;
   }
-  const getName = <T extends { name: string }>(person: T) => {
-    console.log(person.name);
+  const getName = <T extends HasName>(person: T) => {
+    console.log(person.role);
     if ("age" in person) {
       console.log(person.age);
     }
   };
 
-  getName<{ name: string; age: number }>({ name: "Mukit Hossen", age: 25 });
+  getName<{ role: string; age: number }>({ role: "Mukit Hossen", age: 25 });
 }
