d895e6b546faf93473061779e8d9c48168d099e9
MukitHossen7 <hossenmukit7@gmail.com>
2025-05-07 15:20:58 +0600

    Refactor Conditional.ts to comment out unused type definitions and add IsArray type check

--- typescript-practice/module/src/Conditional.ts
+++ typescript-practice/module/src/Conditional.ts
@@ -1,6 +1,22 @@
 {
-  type isString<T> = T extends string ? "Yes" : "No";
-  type A = isString<string>;
-  type B = isString<number>;
-  type C = isString<boolean>;
+  // type isString<T> = T extends string ? "Yes" : "No";
+  // type A = isString<string>;
+  // type B = isString<number>;
+  // type C = isString<boolean>;
+  // type IsObject<T> = T extends object ? "Object" : "Not Object";
+  // type A = IsObject<{}>;
+  // type B = IsObject<number>;
+
+  //ইউনিয়ন টাইপের সাথে ব্যবহার
+  // type ToString<T> = T extends number | boolean ? string : T
+  // type A = ToString<string>
+  // type B = ToString<number>
+
+  //এরেওর বা never টাইপ শনাক্ত করা
+  // type IsNever<T> = [T] extends [never] ? true : false;
+  // type A = IsNever<never>
+
+  //অ্যারে কিনা চেক করা
+  type IsArray<T> = T extends any[] ? true : false;
+  type A = IsArray<string[]>;
 }
