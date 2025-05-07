0d54a62068df15dc0cc7b7e7d87677a302c9980b
MukitHossen7 <hossenmukit7@gmail.com>
2025-05-07 12:15:24 +0600

    Refactor Constraints.ts to comment out unused getName function and its calls

--- typescript-practice/module/src/Constraints.ts
+++ typescript-practice/module/src/Constraints.ts
@@ -12,15 +12,41 @@
   //   getName<{ name: string; age: number }>({ name: "Developer", age: 25 });
 
   //Example : 2
-  interface HasName {
-    role: string;
-  }
-  const getName = <T extends HasName>(person: T) => {
-    console.log(person.role);
-    if ("age" in person) {
-      console.log(person.age);
-    }
-  };
+  //   interface HasName {
+  //     role: string;
+  //   }
+  //   const getName = <T extends HasName>(person: T) => {
+  //     console.log(person.role);
+  //     if ("age" in person) {
+  //       console.log(person.age);
+  //     }
+  //   };
 
-  getName<{ role: string; age: number }>({ role: "Mukit Hossen", age: 25 });
+  //   getName<{ role: string; age: number }>({ role: "Mukit Hossen", age: 25 });
+
+  type Student = {
+    name: string;
+    age: number;
+    email: string;
+    isMarried: boolean;
+    address: {
+      present: string;
+      permanent: string;
+    };
+  };
+  const students = <T, Y extends keyof T>(studentsObj: T, key: Y) => {
+    console.log(studentsObj);
+    console.log(studentsObj[key]);
+  };
+  const student1 = {
+    name: "Mukit Hossen",
+    age: 25,
+    email: "mukit@example.com",
+    isMarried: true,
+    address: {
+      present: "Dhaka, Bangladesh",
+      permanent: "Kushtia, Bangladesh",
+    },
+  };
+  students(student1, "name");
 }
