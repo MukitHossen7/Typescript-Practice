204ff6225b762199e403faae74befeca5c23dedb
Mukit Hossen <hossenmukit7@gmail.com>
2025-07-18 21:17:48 +0600

    Update Person.ts

--- basic-typescript/src/Person.ts
+++ basic-typescript/src/Person.ts
@@ -2,7 +2,7 @@ export class Person {
   private name: string;
   private age: number | string;
   private country: string;
-public is single : boolean;
+public isSingle : boolean;
   constructor(name: string, age: number | string, country: string) {
     this.name = name;
     this.age = age;
