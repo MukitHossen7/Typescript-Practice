e385524a508eb706a67aa90045e371e10b6d9862
Mukit Hossen <hossenmukit7@gmail.com>
2025-07-19 17:35:51 +0600

    Update 2.1.ts

--- typescript-module-2/module/src/2.1.ts
+++ typescript-module-2/module/src/2.1.ts
@@ -3,6 +3,7 @@
   const length = (massage as string).length;
   console.log(length); // Output: 38
 let message2:string = "Hello Developer"
+let message3 = 6*5
 
   let someData: unknown = 12345;
   let num = someData as number;
